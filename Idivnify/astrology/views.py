e31aab0082e0e2ebfef68f3fa42e24df588de3c4
priyanshu parashar <priyanshuparashar223@gmail.com>
2024-11-12 00:08:57 +0530

    horoscope api 12-11-24

--- Idivnify/astrology/views.py
+++ Idivnify/astrology/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+import random
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils.safestring import mark_safe
+from django.utils import timezone
+from .models import *
+from django.http import HttpResponse, JsonResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from datetime import date
+from django.core.cache import cache
 
 # Create your views here.
+def index(request):
+    
+    return HttpResponse("hello")
